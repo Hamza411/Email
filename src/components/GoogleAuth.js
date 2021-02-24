6c4d92fb37028ce0b6af7a5522a9acc81a89bb04
Hamza <hamzasaleam@gmail.com>
2021-02-24 19:16:43 +0500

    day 2

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react'
-import GoogleLogin from 'react-google-login'
+import axios from 'axios';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 
-export class App extends Component {
+class GoogleAuth extends Component {
 
-  responseGoogle(response){
-    console.log(response);
-    console.log(response.profileObj);
-    
-    
-  }
-  render() {
-    return (
-      <div>
-        <GoogleLogin
-        clientId="149249245910-fho3p7j2ih2j0nc3a6tiahutt8ss5ae2.apps.googleusercontent.com"
-        buttonText="Login With Gmail"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        
-        />
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: null,
+            email: null
+        }
+    }
+
+    responseGoogle(response) {
+        console.log("response", response);
+        this.setState({
+            userId: response.profileObj.googleId,
+            email: response.profileObj.email
+        })
+
+        // const userId = response.profileObj.googleId;
+        // const email = response.profileObj.email
+        console.log(this.state.userId);
+        console.log(this.state.email)
+    }
+
+    render() {
+        return (
+            <div>
+                <GoogleLogin
+                    clientId="1075903194307-lgglf5n8qsk3lfhr2j4k4512k60scmh7.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={() => this.responseGoogle()}
+                    onFailure={() => this.responseGoogle()}
+                    cookiePolicy={'single_host_origin'}
+                    scope="https://www.googleapis.com/auth/gmail.labels"
+                    key="AIzaSyDqTkgLrbg4aJy9bPhT2iL5ETNJ43y6CB4"
+                />
+            </div>
+        )
+    }
 }
 
-export default App
+export default GoogleAuth
